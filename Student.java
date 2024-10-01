746746cd43044521248a6ddc031f2930d202c5cf
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-01 20:09:26 +0800

    "Added fields to Student class: studentID, name, type, and courses"

--- Student.java
+++ Student.java
@@ -1,3 +1,6 @@
 public abstract class Student {
-     
+    protected String studentID;
+    protected String name;
+    protected String type; // regular or irregular
+    protected List<Course> courses;
 }
