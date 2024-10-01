6576573dc2848aa7755a6632763600c897f47c91
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-02 01:53:23 +0800

    Updated Student class constructor to accept type, added admissionFee field, and modified getDetails method to include admissionFee. Added setAdmissionFee, updateType, and getAdmissionFee methods.

--- Student.java
+++ Student.java
@@ -7,15 +7,17 @@ public abstract class Student {
     protected String name;
     protected String type; // regular or irregular
     protected List<Course> courses;
+    protected double admissionFee;
 
-    public Student(String studentID, String name) {
+    public Student(String studentID, String name, String type) {
         this.studentID = studentID;
         this.name = name;
+        this.type = type;
         this.courses = new ArrayList<>();
+        this.admissionFee = setAdmissionFee(type);
     }
 
     // methods
-
     public List<Course> getCourses() {
         return courses;
     }
@@ -25,11 +27,7 @@ public abstract class Student {
     }
 
     public String getDetails() {
-        return "Name: " + name + "\nID: " + studentID + "\nType: " + type;
-    }
-
-    public void updateType(String type) {
-        this.type = type;
+        return "Name: " + name + ", ID: " + studentID + ", Type: " + type + ", Admission Fee: " + admissionFee;
     }
 
     public void dropCourse(Course course) {
@@ -46,6 +44,23 @@ public abstract class Student {
             grades.add(course.getGradeForStudent(this));
         }
         return grades;
+    }
+
+    private double setAdmissionFee(String type) {
+        if (type.equalsIgnoreCase("Regular")) {
+            return 10000.0;
+        } else {
+            return 10000.0 + 2000.0; 
+        }
+    }
+
+    public void updateType(String newType) {
+        this.type = newType;
+        this.admissionFee = setAdmissionFee(newType); // update the fee based on new type
+    }
+
+    public double getAdmissionFee() {
+        return admissionFee;
     }
 
     public abstract double calculateTotalFees();
