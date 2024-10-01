bce8d8fff8165a45db9d3070fb88d877ffbffef2
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-01 22:02:43 +0800

    created a method for regular students enrolling in a block that already has a predefined schedule

--- RegularStudent.java
+++ RegularStudent.java
@@ -5,6 +5,7 @@ public class RegularStudent extends Student {
         super(studentID, name);
         this.type = "Regular";
         this.block = block;
+        assignPredefinedCourses();
     }
 
     // method to retrieve block
@@ -12,7 +13,34 @@ public class RegularStudent extends Student {
         return block;
     }
 
+    // a private method of predefined schedules for reg students
+    private void assignPredefinedCourses() {
+        if (block.equals("A")) {
+            enroll(new Course("Introduction to Web Programming", "2067", "Thursday", "8:30 AM"));
+            enroll(new Course("Introduction to Cyber Security", "2069", "Friday", "11:00 AM"));
+        } else if (block.equals("B")) {
+            enroll(new Course("Data Structures & Algorithms", "2065", "Monday", "12:00 PM"));
+            enroll(new Course("Object-Oriented Programming", "2066", "Tuesday", "4:00 PM"));
+        } else if (block.equals("C")) {
+            enroll(new Course("Operating Systems Fundamentals", "2068", "Wednesday", "9:00 AM"));
+            enroll(new Course("Calculus", "2070", "Thursday", "11:00 AM"));
+        } else if (block.equals("D")) {
+            enroll(new Course("Physical Education 3", "9211", "Friday", "2:00 PM"));
+            enroll(new Course("Ethics", "2071", "Monday", "3:00 PM"));
+        }
+    }
+
+    @Override
     public void enroll(Course course) {
-        System.out.println("Regular students cannot enroll in additional courses."); // message will print out - regular students have predefined schedule
+        if (courses.size() >= 2) {
+            System.out.println("Regular students are already enrolled in predefined courses for their block and cannot enroll in additional courses."); // message will print out - regular students have predefined schedule
+        } else {
+            super.enroll(course);
+        }
+    }
+
+    @Override
+    public double calculateTotalFees() {
+        return 10000.0; // standard fee
     }
 }
