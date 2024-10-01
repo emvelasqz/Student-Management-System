c8e09250dbbaf03e9ea28fd7fcafd80e7cdb8b0d
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-01 23:46:14 +0800

    created methods for schedule class

--- Schedule.java
+++ Schedule.java
@@ -1,3 +1,39 @@
 public class Schedule {
-    
+    private String day;
+    private String time;
+    private String block;
+
+    public Schedule(String day, String time, String block) {
+        this.day = day;
+        this.time = time;
+        this.block = block;
+    }
+
+    public String getScheduleDetails() {
+        return "Day: " + day + "\nTime: " + time + "\nBlock: " + block;
+    }
+
+    public void updateTime(String newTime) {
+        this.time = newTime;
+    }
+
+    public void updateDay(String newDay) {
+        this.day = newDay;
+    }
+
+    public void updateBlock(String newBlock) {
+        this.block = newBlock;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getBlock() {
+        return block;
+    }
 }
