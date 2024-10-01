22272f01c531f9208721a321efa873b21a756e36
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-01 22:08:47 +0800

    added constructor and overridden calculateTotalFees method to IrregularStudent class

--- IrregularStudent.java
+++ IrregularStudent.java
@@ -1,3 +1,14 @@
 class IrregularStudent extends Student {
-    
+    private static final double additionalAdmissionFee = 2000.0; // a constant value for the additional fee
+
+    // constructor
+    public IrregularStudent(String studentID, String name) {
+        super(studentID, name);
+        this.type = "Irregular";
+    }
+
+    @Override // to override the fixed calculation
+    public double calculateTotalFees() {
+        return 10000.0 + additionalAdmissionFee; // standard fee + additional fee
+    }
 }
