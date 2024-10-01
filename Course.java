c577ccc33a8bc67bf2eb5a9cd087f6c232d8b004
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-01 22:10:48 +0800

    created Course class and defined the attributes

---
+++ Course.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+    private String courseName;
+    private String courseCode;
+    private String day;
+    private String time;
+    private List<Grade> grades;
+}
