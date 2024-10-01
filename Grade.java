a8c79c07085618022c7313317ab6476d9ca7ef2c
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-01 22:25:23 +0800

    added constructors and getter methods

--- Grade.java
+++ Grade.java
@@ -1,3 +1,25 @@
 public class Grade {
+    private Course course;
+    private double gradeValue;
+    private Student student;
+
+    // constructor
+    public Grade(Course course, Student student, double gradeValue) {
+        this.course = course;
+        this.student = student;
+        this.gradeValue = gradeValue;
+    }
+
+    // metthods (getter)
+    public Course getCourse() {
+        return course;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
     
+    public double getGradeValue() {
+        return gradeValue;
+    }
 }
