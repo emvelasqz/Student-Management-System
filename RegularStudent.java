39240c493ad25af217d9c5b8ef80a9bfa42b8d43
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-01 21:21:25 +0800

    attributes and methods for regularstudent class

---
+++ RegularStudent.java
@@ -0,0 +1,17 @@
+public class RegularStudent extends Student {
+    private String block;
+
+    public RegularStudent(String studentID, String name, String block) {
+        super(studentID, name);
+        this.block = block;
+    }
+
+    // method to retrieve block
+    public getBlock() {
+        return block;
+    }
+
+    public void enroll(Course course) {
+        System.out.println("Regular students cannot enroll in additional courses."); // message will print out - regular students have predefined schedule
+    }
+}
