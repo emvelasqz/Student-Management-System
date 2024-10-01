6cd01a00ad0ff223e2fc627e3163ecbc119ff3c3
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-01 22:34:08 +0800

    added a method to create a student

--- Inputter.java
+++ Inputter.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Inputter {
-    
+    public Scanner scan = new Scanner(System.in);
+
+    public Student createStudent() {
+        System.out.println("Enter Student ID: ");
+        String studentID = scan.nextLine();
+        System.out.println("Enter Student Name: ");
+        String name = scan.nextLine();
+        System.out.println("Enter Student Type (Regular/Irregular): ");
+        String type = scan.nextLine();
+
+        // if-else condition to determine student type
+        if (type.equalsIgnoreCase("Regular")) {
+            System.out.println("Enter Block (A to D):"); // to enroll in a block
+            String block = scan.nextLine();
+            return new RegularStudent(studentID, name, block); // assign the student in the chosen block
+        } else {
+            return new IrregularStudent(studentID, name);
+        }
+    }
 }
