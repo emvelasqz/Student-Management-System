61b9149384cb0d24f284f641d9ab0a38262e7222
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-08 23:19:52 +0800

    changed `block.equals()` to `block.equalsIgnoreCase()` in `assignPredefinedCourses()` method of `RegularStudent` class

--- RegularStudent.java
+++ RegularStudent.java
@@ -14,7 +14,7 @@ public class RegularStudent extends Student {
 
     // a private method of predefined schedules for reg students
     private void assignPredefinedCourses() {
-        if (block.equals("A")) {
+        if (block.equalsIgnoreCase("A")) {
             Course course1 = new Course("Introduction to Web Programming", "2067", null);
             course1.addSchedule(new Schedule("Thursday", "8:30 AM", "A"));
             enroll(course1);
@@ -22,7 +22,7 @@ public class RegularStudent extends Student {
             Course course2 = new Course("Introduction to Cyber Security", "2069", null);
             course2.addSchedule(new Schedule("Friday", "11:00 AM", "A"));
             enroll(course2);
-        } else if (block.equals("B")) {
+        } else if (block.equalsIgnoreCase("B")) {
             Course course1 = new Course("Data Structures & Algorithms", "2065", null);
             course1.addSchedule(new Schedule("Monday", "12:00 PM", "B"));
             enroll(course1);
@@ -30,7 +30,7 @@ public class RegularStudent extends Student {
             Course course2 = new Course("Object-Oriented Programming", "2066", null);
             course2.addSchedule(new Schedule("Tuesday", "4:00 PM", "B"));
             enroll(course2);
-        } else if (block.equals("C")) {
+        } else if (block.equalsIgnoreCase("C")) {
             Course course1 = new Course("Operating Systems Fundamentals", "2068", null);
             course1.addSchedule(new Schedule("Wednesday", "9:00 AM", "C"));
             enroll(course1);
@@ -38,7 +38,7 @@ public class RegularStudent extends Student {
             Course course2 = new Course("Calculus", "2070", null);
             course2.addSchedule(new Schedule("Thursday", "11:00 AM", "C"));
             enroll(course2);
-        } else if (block.equals("D")) {
+        } else if (block.equalsIgnoreCase("D")) {
             Course course1 = new Course("Physical Education 3", "9211", null);
             course1.addSchedule(new Schedule("Friday", "2:00 PM", "D"));
             enroll(course1);
