6c89814432faf0dd4749cf5f2b74c965bea9244d
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-23 23:02:43 +0800

    removed fields and methods related to grades and schedules from Course class

--- Course.java
+++ Course.java
@@ -2,19 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Course {
+    private Course course;
     protected List<Course> courses;
     protected String courseName;
     protected String courseCode;
-    protected List<Schedule> schedules;
-    protected List<Grade> grades;
 
     // constructor
     public Course(String courseName, String courseCode, Schedule schedule) {
         this.courses = new ArrayList<>();
         this.courseName = courseName;
         this.courseCode = courseCode;
-        this.grades = new ArrayList<>();
-        this.schedules = new ArrayList<>();
     }
 
     // methods
@@ -32,22 +29,6 @@ public class Course {
 
     public void dropCourse(Course course) {
         courses.remove(course);
-    }
-
-
-
-    public void assignGrade(Student student, double gradeValue) {
-        Grade grade = new Grade(this, student, gradeValue);
-        grades.add(grade);
-    }
-
-    public Grade getGradeForStudent(Student student) {
-        for (Grade grade : grades) {
-            if (grade.getStudent().equals(student)) {
-                return grade;
-            }
-        }
-        return null;
     }
 
     // to display the details of the course
