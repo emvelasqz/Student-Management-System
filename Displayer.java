00266e667808bc39e369d0ce066f560035633a81
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-25 21:09:14 +0800

    refactored Displayer class: removed comments, rearranged and simplified display methods for course and student details

--- Displayer.java
+++ Displayer.java
@@ -1,26 +1,19 @@
 public class Displayer {
-    // methods to display in the main class
-    public void displayStudentDetails(Student student) {
-        System.out.println(student.getDetails());
-        
-        System.out.println("Enrolled Courses:");
-        for (Course course : student.getCourses()) {
-            Grade grade = course.getGradeForStudent(student);
-            if (grade != null) {
-                System.out.println(course.getDetails() + " - Grade: " + grade.getGradeValue());
-            } else {
-                System.out.println(course.getDetails() + " - Grade: Not assigned");
-            }
-
-            System.out.println("Course Schedule:");
-            course.viewSchedules();
-        }
-        System.out.println("Total Amount: " + student.calculateTotalFees());
-    }
-
     public void displayCourseDetails(Course course) {
         System.out.println(course.getDetails());
         System.out.println("Schedules:");
         course.viewSchedules();
+    }
+
+    public void displayStudentDetails(Student student) {
+        System.out.println(student.getDetails());
+        System.out.println("Courses Enrolled:");
+        for (Course course : student.getCourses()) {
+            Grade grade = course.getGradeForStudent(student);
+            String gradeText = (grade != null) ? String.valueOf(grade.getGradeValue()) : "Not assigned";
+            System.out.println(course.getDetails() + " - Grade: " + gradeText);
+            System.out.println("Course Schedules:");
+            course.viewSchedules();
+        }
     }
 }
