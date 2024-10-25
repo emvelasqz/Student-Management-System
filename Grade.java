bf1b1931f6f9278fe3ad26046b571e05ed93bf92
Erika Marie Velasquez <emvelasqz@gmail.com>
2024-10-25 21:10:00 +0800

    removed unnecessary imports, fields, and methods from Grade class; refactored constructor and getters

--- Grade.java
+++ Grade.java
@@ -1,50 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Grade {
+    private final Course course;
+    private final Student student;
     private double gradeValue;
-    private Student student;
-    protected List<Grade> grades;
 
-    // constructor
     public Grade(Course course, Student student, double gradeValue) {
+        this.course = course;
         this.student = student;
         this.gradeValue = gradeValue;
-    }
-
-    // methods (getter)
-
-    public Student getStudent() {
-        return student;
     }
 
     public double getGradeValue() {
         return gradeValue;
     }
 
-    public Grade getGradeForStudent(Student student) {
-        for (Grade grade : grades) {
-            if (grade.getStudent().equals(student)) {
-                return grade;
-            }
-        }
-        return null;
-    }
-
-    public List<Grade> getGrades() {
-        List<Grade> grades = new ArrayList<>();
-        for (Course course : courses) {
-            grades.add(course.getGradeForStudent(this));
-        }
-        return grades;
-    }
-
-    public void assignGrade(Student student, double gradeValue) {
-        Grade grade = new Grade(this, student, gradeValue);
-        grades.add(grade);
-    }
-
     public void setGradeValue(double gradeValue) {
         this.gradeValue = gradeValue;
+    }
+
+    public Student getStudent() {
+        return student;
     }
 }
